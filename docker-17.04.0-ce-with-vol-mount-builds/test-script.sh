cdedf66decedafdd88759be5862205c813ba24af
Christopher Jones <tophj@linux.vnet.ibm.com>
2017-07-17 12:28:38 -0400

    added vol-mount build to 17.04.0
    Signed-off-by: Christopher Jones <tophj@linux.vnet.ibm.com>

---
+++ docker-17.04.0-ce-with-vol-mount-builds/test-script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+apt-get update && apt-get install -y docker-engine
+
+git clone https://github.com/tophj-ibm/moby.git
+cd moby
+git fetch --all
+git checkout unicamp-v17.04.0-ce-with-volume-mount-builds
+DOCKER_BUILD_PKGS=ubuntu-trusty make deb
+echo " - - - - - - - - - - - - - - - - - - - - "
+echo ""
+echo " The final 14.04 deb should be located in : "
+echo " docker/bundles/latest/build-deb/ubuntu-trusty/*.deb "
+echo ""
+echo " Feel free to copy this out to target destination"
+echo ""
+echo " - - - - - - - - - - - - - - - - - - - - "
+
